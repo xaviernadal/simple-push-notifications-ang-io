d088bc4302f9e6ffae7a3e1af04b60bec3b7d9e6
xaviernadal <xavinadal14@gmail.com>
2022-07-05 11:35:06 +0200

    Notifications working with firebase messaging

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,14 @@
+/*
+https://capacitorjs.com/docs/guides/push-notifications-firebase#sending-a-test-notification
+*/
+
 import { Component } from '@angular/core';
+import {
+  ActionPerformed,
+  PushNotificationSchema,
+  PushNotifications,
+  Token,
+} from '@capacitor/push-notifications';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +17,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+
   constructor() {}
 
+  ngOnInit() {
+    PushNotifications.requestPermissions().then(result => {
+      if (result.receive === 'granted') {
+        PushNotifications.register();
+      } else {
+        console.log('Permission not granted');
+      }
+  });
+  
+    PushNotifications.addListener('registration', (token: Token) => {
+      alert('Push registration success, token: ' + token.value);
+    });
+
+    PushNotifications.addListener('registrationError', (error: any) => {
+      alert('Error on registration: ' + JSON.stringify(error));
+    });
+
+    PushNotifications.addListener('pushNotificationReceived', (notification: PushNotificationSchema) => {
+      alert('Push received: ' + JSON.stringify(notification));
+    });
+
+    PushNotifications.addListener('pushNotificationActionPerformed', (notification: ActionPerformed) => {
+      alert('Push action performed: ' + JSON.stringify(notification));
+    });
+  }
 }
